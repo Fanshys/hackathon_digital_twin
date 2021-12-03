5b4ece5c46b1678beb16069a27e18a62d352b883
Lev Kaplunov <limon4ik59rus@mail.ru>
2021-12-04 01:56:51 +0500

    Готова основа детального ресурса

--- client/src/components/ResourceDetail/ResourceDetail.vue
+++ client/src/components/ResourceDetail/ResourceDetail.vue
@@ -10,19 +10,59 @@
 		</div>
 
 		<div class="resources-detail__info">
-			
+			<img v-if="img" :src="img" class="resources-detail__img">
+
+			<div v-if="date" class="resources-detail__date">
+				{{ date }}
+			</div>
+
+			<h2 v-if="title" class="resources-detail__title">
+				{{ title }}
+			</h2>
+
+			<a class="resources-detail__link" v-if="link" :href="link">{{ link }}</a>
+
+			<div v-if="description" v-html="description" class="resources-detail__description"></div>
+
+			<WtButton v-if="link" :to="link">Перейти на источник</WtButton>
 		</div>
 	</div>
 </template>
 
 <script>
+import WtButton from "../ui/WtButton/WtButton";
 export default {
 	name: "ResourceDetail",
-
+	components: {WtButton},
 	props: {
 		isEmpty: {
 			type: Boolean,
 			default: true,
+		},
+
+		img: {
+			type: String,
+			default: null
+		},
+
+		title: {
+			type: String,
+			default: null
+		},
+
+		link: {
+			type: String,
+			default: null
+		},
+
+		description: {
+			type: String,
+			default: null
+		},
+
+		date: {
+			type: String,
+			default: null
 		}
 	}
 }
