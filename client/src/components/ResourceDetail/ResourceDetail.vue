fd21ecd02a49aea2637de80dd855e08cac4280b1
Lev Kaplunov <limon4ik59rus@mail.ru>
2021-12-04 01:13:17 +0500

    Начал делать детальную ресурса

---
+++ client/src/components/ResourceDetail/ResourceDetail.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="resources-detail">
+		<div v-if="isEmpty" class="resources-detail__empty">
+			<div class="resources-detail__icon">👈</div>
+			<div class="resources-detail__text">
+				Здесь будет подробная информация
+				об источнике. Выберите источник влевой
+				части экрана.
+			</div>
+		</div>
+
+		<div class="resources-detail__info">
+			
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ResourceDetail",
+
+	props: {
+		isEmpty: {
+			type: Boolean,
+			default: true,
+		}
+	}
+}
+</script>
+
+<style scoped lang="scss" src="./ResourceDetail.scss"></style>
--- client/src/views/HomePage/HomePage.vue
+++ client/src/views/HomePage/HomePage.vue
@@ -4,17 +4,18 @@
 		<div class="main-layout__content">
 			<TheContent />
 		</div>
-		<div class="main-layout__detail-content">detail</div>
+		<div class="main-layout__detail-content"><ResourceDetail /></div>
   </div>
 </template>
 
 <script>
 import TheSidebar from "../../components/TheSidebar/TheSidebar";
 import TheContent from "../../components/TheContent/TheContent";
+import ResourceDetail from "../../components/ResourceDetail/ResourceDetail";
 
 export default {
   name: 'HomePage',
-	components: {TheSidebar, TheContent},
+	components: {ResourceDetail, TheSidebar, TheContent},
 };
 </script>
 
