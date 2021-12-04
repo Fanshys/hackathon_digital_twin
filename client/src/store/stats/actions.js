9058b0fd00fa825440dbe377d746e211c94052bd
Lev Kaplunov <limon4ik59rus@mail.ru>
2021-12-04 05:20:11 +0500

    сделал проверку на прод, фикс обращения не по тому url

--- client/src/store/stats/actions.js
+++ client/src/store/stats/actions.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+import rootUrl from "../rootUrl";
 
 export default {
   async getStats({commit}, payload) {
     commit('setStatsIsLoading', true);
 
-    const { data } = await axios.post('http://localhost:3000/api/stats/', {userInfo: payload});
+    const { data } = await axios.post(`${rootUrl}api/stats/`, {userInfo: payload});
     commit('setStats', data.body);
     commit('setStatsIsLoading', false);
   }
