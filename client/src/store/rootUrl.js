a356735817ffdfcc235b354185393f859d8e8bac
Lev Kaplunov <limon4ik59rus@mail.ru>
2021-12-04 09:18:02 +0500

    Мелкие баги

--- client/src/store/rootUrl.js
+++ client/src/store/rootUrl.js
@@ -1,4 +1,4 @@
-let rootUrl = 'http://0.0.0.0';
+let rootUrl = 'http://45.134.255.154:30214';
 
 if (typeof process !== 'undefined' && process?.env?.NODE_ENV === 'development') {
 	rootUrl = 'http://localhost:3000/';
